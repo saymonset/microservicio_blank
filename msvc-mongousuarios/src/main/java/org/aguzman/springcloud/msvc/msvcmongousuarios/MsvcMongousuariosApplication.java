3f45357dce76a66c66862cf9862f511d0481b85e
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2023-04-05 09:18:55 -0400

    Mongo Usuarios microservicio

---
+++ msvc-mongousuarios/src/main/java/org/aguzman/springcloud/msvc/msvcmongousuarios/MsvcMongousuariosApplication.java
@@ -0,0 +1,13 @@
+package org.aguzman.springcloud.msvc.msvcmongousuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcMongousuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcMongousuariosApplication.class, args);
+	}
+
+}
---
+++ msvc-mongousuarios/src/test/java/org/aguzman/springcloud/msvc/msvcmongousuarios/MsvcMongousuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aguzman.springcloud.msvc.msvcmongousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcMongousuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
